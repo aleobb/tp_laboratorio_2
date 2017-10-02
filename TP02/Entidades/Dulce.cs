b947810e1b29d478bda34c0d2b49d5826a58ec3d
aleobb <aleobb@gmail.com>
2017-10-01 23:41:15 -0300

    pre-presentacion

---
+++ TP02/Entidades/Dulce.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2017
+{
+    public class Dulce : Producto
+    {
+        /// <summary>
+        /// Constructor que reutiliza el constructor de la base
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="codigo"></param>
+        /// <param name="color"></param>
+        public Dulce(EMarca marca, string codigo, ConsoleColor color): base(codigo, marca, color)
+        {
+        }
+
+        /// <summary>
+        /// Los dulces tienen 80 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 80;
+            }
+        }
+
+        /// <summary>
+        /// Metodo privado por el cual se genera un string con todos los datos del objeto
+        /// </summary>
+        /// <returns></returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("DULCE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("CALORIAS : {0}", this.CantidadCalorias);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
